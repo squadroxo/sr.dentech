f39a6a2c0057c5a83a359782aad1b4fa97e3d747
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-16 11:36:15 -0300

    removing test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Should have title', () => {
-  render(<App />);
-  const titulo = screen.getByText("DaniDev")
-  expect(titulo).toBeInTheDocument();
-});
