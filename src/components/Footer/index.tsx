b4c4f7e4300c4ca480cc7196f0ff78a411f076d2
Daniela Alves <danieladossantosalves01@gmail.com>
2024-08-05 16:39:24 -0300

    fix: footer redirecionamento

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,13 +4,20 @@ import styles from "./footer.module.scss"
 export const Footer = () => {
   const handleClick = () => {
     
-  }
+};
   return(
     <footer className={styles.container}>
       <h3 className={styles.container__contato}>Contato: <span>e-mailsrdente@e-mail.com</span></h3>
       <div className={styles.container__contSec}>
         <p className={styles.container__contSec__txt}>Essa é uma iniciativa voluntária em parceria com o Pipoca Ágil</p>
-        <Button label="Pipoca Ágil" link="/" onClick={handleClick} primario={false} className={styles.container__contSec__btn} labelClassName={styles.container__contSec__label}/>
+        <Button  
+        label="Pipoca Ágil" 
+        link="/" 
+        onClick={() => { 
+          console.log('Botão clicado!'); 
+          window.open('https://pipocaagil.com.br', '_blank'); 
+      }} 
+         primario={false} className={styles.container__contSec__btn} labelClassName={styles.container__contSec__label}/>
       </div>
     </footer>
   )
