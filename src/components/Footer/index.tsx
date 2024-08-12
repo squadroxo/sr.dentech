18b4fdaa083b4b099efd3c5625a7e3034f2d1401
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-12 16:15:01 -0300

    solving conflicts

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,12 @@
 import Button from "components/ContainerButton"
 import styles from "./footer.module.scss"
-import { useLocation } from "react-router"
 
 export const Footer = () => {
-  const local = useLocation()
-  const homePage = local.pathname === "/" ? true : false;
-  
-
   const handleClick = () => {
     
   }
   return(
-    <footer 
-      className={homePage ? styles.container : styles.containerSecPages}
-    >
+    <footer className={styles.container}>
       <h3 className={styles.container__contato}>Contato: <span>e-mailsrdente@e-mail.com</span></h3>
       <div className={styles.container__contSec}>
         <p className={styles.container__contSec__txt}>Essa é uma iniciativa voluntária em parceria com o Pipoca Ágil</p>
