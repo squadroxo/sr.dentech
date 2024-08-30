05fcd192eb71896b1c1b642c6166c09cd2a7fb92
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-30 10:19:38 -0300

    refactor: changing sass -> styled components

--- src/components/LetsMake/index.tsx
+++ src/components/LetsMake/index.tsx
@@ -1,11 +1,13 @@
 import line from "../../assets/line.svg";
 import styles from "./LetsMake.module.scss";
+import * as Style from "./styles";
+
 export const LetsMake = () => {
   return (
-    <div className={styles.container}>
-      <h3 className={styles.subtitle}>Vamos construir algo incrível juntos!</h3>
-      <p className={styles.desc}>Estamos desenvolvendo uma página web super interativa e educativa para ajudar você a cuidar da saúde dental da sua família de maneira fácil e divertida. Imagine um mundo onde todos têm sorrisos brilhantes e saudáveis!</p>
+    <Style.Container>
+      <h3 className="subtitle">Vamos construir algo incrível juntos!</h3>
+      <p className="desc">Estamos desenvolvendo uma página web super interativa e educativa para ajudar você a cuidar da saúde dental da sua família de maneira fácil e divertida. Imagine um mundo onde todos têm sorrisos brilhantes e saudáveis!</p>
       <img src={line} alt="Linha divisória" />
-    </div>
+    </Style.Container>
   )
 }
---
+++ src/components/LetsMake/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Kalam:wght@300;400;700&display=swap');
+  margin: 18px 39px 24px 39px;
+
+  .subtitle{
+  font-weight: 700;
+  font-family: "Kalam", cursive;
+  font-size: 26px;
+  line-height: 33px;
+  color: #21C9D3;
+}
+
+.desc{
+  font-size: 12px;
+  line-height: 16px;
+  margin: 9px 0 24px 0;
+}
+`
