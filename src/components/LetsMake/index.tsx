a917474928af6b4278223f9eb7b7e68d0a080b91
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-30 11:21:40 -0300

    fix: adjusting alt

--- src/components/LetsMake/index.tsx
+++ src/components/LetsMake/index.tsx
@@ -7,7 +7,7 @@ export const LetsMake = () => {
     <Style.Container>
       <h3 className="subtitle">Vamos construir algo incrível juntos!</h3>
       <p className="desc">Estamos desenvolvendo uma página web super interativa e educativa para ajudar você a cuidar da saúde dental da sua família de maneira fácil e divertida. Imagine um mundo onde todos têm sorrisos brilhantes e saudáveis!</p>
-      <img src={line} alt="Linha divisória" />
+      <img src={line} alt="Linha divisória azul com curvas" />
     </Style.Container>
   )
 }
