62aa07c6ded31bcd8c0d12eae2b7f346741bb19f
Daniela Alves <danieladossantosalves01@gmail.com>
2024-09-14 17:47:56 -0300

    feat:menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,44 @@
-  import styles from "./Header.module.scss";
-  import logo from "../../assetsIcons/logoSrDente.png";
-  import { dataLinks } from "./data"
-  import { useNavigate } from "react-router";
-  import { useState } from "react";
-  import { MenuMobile } from "../MenuMobile";
-  import { MenuBurguer } from "../Icons/MenuBurguer";
-  import { AnimatePresence } from "framer-motion"
-  import iconClosed from "../../assets/iconClosed.svg";
-  export const Header = () => {
-    const navigate = useNavigate();
+import styles from "./Header.module.scss";
+import logo from "../../assetsIcons/logoSrDente.png";
+import { dataLinks } from "./data"
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import { MenuMobile } from "../MenuMobile";
+import { MenuBurguer } from "../Icons/MenuBurguer";
+import { AnimatePresence, motion } from "framer-motion";
+import iconClosed from "../../assets/iconClosed.svg";
 
-    const [isOpen, setIsOpen] = useState(false);
+export const Header = () => {
+  const navigate = useNavigate();
 
-    return (
-      <header className={styles.container}>
-        <div className={styles.containerTitulo} onClick={() => navigate("/")}>
-          <img src={logo} alt="Imagem de um dente acenando" className={styles.containerTitulo__img} />
-        </div>
-        <nav className={styles.containerLinks}>
-          <ul className={styles.containerLinks__lista}>
-            {dataLinks.map(link => (
-              <li key={link.id}><a href={link.href} className={styles.containerLinks__lista__item}>{link.texto}</a></li>
-            ))}
-          </ul>
-          <AnimatePresence>
-            {isOpen && <MenuMobile />}
-          </AnimatePresence>
-          <button onClick={() => setIsOpen(pv => !pv)} className={styles.iconMenu}>
-            {isOpen ? (
-              <>
-                <img className={iconClosed} src={iconClosed} alt="Icon closed menubar" />
-              </>
-            ) : (
-              <>
-                <MenuBurguer />
-              </>
-            )}
-          </button>
+  const [isOpen, setIsOpen] = useState(false);
 
-        </nav>
-      </header>
-    )
-  }
+  return (
+    <header className={styles.container}>
+      <div className={styles.containerTitulo} onClick={() => navigate("/")}>
+        <img src={logo} alt="Imagem de um dente acenando" className={styles.containerTitulo__img} />
+      </div>
+      <nav className={styles.containerLinks}>
+        <ul className={styles.containerLinks__lista}>
+          {dataLinks.map(link => (
+            <li key={link.id}><a href={link.href} className={styles.containerLinks__lista__item}>{link.texto}</a></li>
+          ))}
+        </ul>
+        <AnimatePresence>
+          {isOpen && <MenuMobile />}
+        </AnimatePresence>
+        <button onClick={() => setIsOpen(pv => !pv)} className={styles.iconMenu}>
+          {isOpen ? (
+            <>
+              <img className={styles.iconClosed} src={iconClosed} alt="icon closed menubar" />
+            </>
+          ) : (
+            <>
+              <MenuBurguer />
+            </>
+          )}
+        </button>
+      </nav>
+    </header>
+  )
+}
