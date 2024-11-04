ca259dc56cf55dcd1962ea26f241a5a6717dd848
Camilla <milla_mendes83@hotmail.com>
2024-11-04 16:35:47 -0300

    feat: task 108

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 import { useState } from "react";
 import { MenuMobile } from "../MenuMobile";
 import { MenuBurguer } from "../Icons/MenuBurguer";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { IoMdClose } from "react-icons/io";
 
 import * as Styles from "./styles"; 
