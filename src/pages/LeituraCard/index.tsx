bf4bb7c2a52c8109015c6ae9a90327ae715b2549
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2024-11-26 15:04:02 -0300

    feat: task-146 - display list of instructions

--- src/pages/LeituraCard/index.tsx
+++ src/pages/LeituraCard/index.tsx
@@ -1,4 +1,6 @@
 import * as Styles from './styles';
+import InstructionCard from '../../components/InstructionCard';
+import { instructions } from './mockData';
 
 export default function LeituraCard() {
   return (
@@ -9,10 +11,23 @@ export default function LeituraCard() {
         transition={{ duration: 0.3 }}
       >
         {/* Seção 1 - motion */}
-        {/* Seção 2 - motion */}
+
+        <Styles.Instructions
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+        >
+          <h2>Como introduzir o fio dental:</h2>
+          {instructions.map((item, index) => <InstructionCard {...item} key={index} />)}
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        </Styles.Instructions>
+
         {/* Texto em destaque */}
+
       </Styles.Article>
+
       {/* Linha divisória */}
+      
       {/* Article Veja Também (Seção 3) - motion */}
     </>
   );
