1397ed28fcbdf3a2b99d6ef2ddaca686b349a916
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2024-11-26 11:03:42 -0300

    feat: task-144 - add motion props

--- src/pages/LeituraCard/index.tsx
+++ src/pages/LeituraCard/index.tsx
@@ -3,13 +3,17 @@ import * as Styles from './styles';
 export default function LeituraCard() {
   return (
     <>
-      <Styles.Article>
-        {/* Seção 1 */}
-        {/* Seção 2 */}
+      <Styles.Article
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        {/* Seção 1 - motion */}
+        {/* Seção 2 - motion */}
         {/* Texto em destaque */}
       </Styles.Article>
       {/* Linha divisória */}
-      {/* Article Veja Também (Seção 3) */}
+      {/* Article Veja Também (Seção 3) - motion */}
     </>
   );
 }
